4cb51655692624c3f793234e7129b745fec2a33e
prakashu <prakashu@thoughtworks.com>
2018-02-09 23:18:09 +0530

    Updated card structure and styling

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,12 +2,21 @@ import React, {Component} from 'react';
 import './card.css';
 
 class Card extends Component {
-  render() {
-    return (
-    	<div className="card">
-			   <h1>Card 1</h1>
-    	</div>
-    );
+      render() {
+        return (
+            <div className="card">
+                <div className="card-info">
+                    <div className="card-info__ranking">
+                        <i className="material-icons like-icon u-pointer">favorite</i>
+                        <div>5</div>
+                    </div>
+                    <h3 className="card-info__title">Rocket raccon</h3>
+                </div>
+                <div className="card-image">
+                    <img src={'http://via.placeholder.com/350x150'}/>
+                </div>
+            </div>
+        );
+      }
   }
-}
 export default Card;
